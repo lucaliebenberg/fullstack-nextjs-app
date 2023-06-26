14c2cfe5c4f20c3ac6bacf2e4489cac4f1e07aa3
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-06-26 09:07:05 +0200

    project setup commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "31ac68cdda34a6d0"; }
+/******/ 		__webpack_require__.h = function() { return "684ab429f2fae951"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
